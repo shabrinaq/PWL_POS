1b0351e535ba13164bbce8276f8714aa340e7395
shabrinaq <shabrinaqottrunnada@gmail.com>
2025-03-09 15:14:50 +0700

    Practicum 2.6_Create, Read, Update, Delete(CRUD)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,44 +10,47 @@ class UserController extends Controller
 {
     public function index()
     {   
-    /*  $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
         ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request){
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->level_id = $request->level_id;
+
         $user->save();
-        
-        $user->username = 'manager56';
-        
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty(['nama', 'username']); // true
-        
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // true
-        $user->isClean(['nama', 'username']); // false
-        $user->save();
-        
-        $user->isDirty(); // false
-        $user->isClean(); // true
-        dd($user->isDirty()); */
-        
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-        $user->username = 'manager12';
-        $user->save();
-        
-        $user->wasChanged(); //true
-        $user->wasChanged('username'); //true
-        $user->wasChanged(['username', 'level_id']); //true
-        $user->wasChanged('nama'); //false
-        dd($user->wasChanged(['nama', 'username'])); //true
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
