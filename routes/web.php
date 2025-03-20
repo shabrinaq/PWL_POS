6347334adad6296882796ac9f68355b0126fba08
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2025-03-20 11:02:19 +0700

    JS5 - Practicum 3_Implementation of jQuery Datatable

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index'])->name('/user');
-
-Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);  // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);  // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);  // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);  // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);  // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);  // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
+});
