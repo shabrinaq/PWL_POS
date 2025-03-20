e3e972123041f6b366532486bec1cda05c69083b
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2025-03-20 11:30:01 +0700

    JS5 - Practicum 4_Implementation of Filtering Datatables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,18 +22,27 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
         return view('user.index', [
             'breadcrumb' => $breadcrumb, 
             'page' => $page, 
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
 
+    // Ambil data user dalam bentuk json untuk datatable
     public function list(Request $request)
     {
     $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
+    // Filter data user berdasarkan level_id
+    if ($request->level_id) {
+        $users->where('level_id', $request->level_id);
+    }
+    
     return DataTables::of($users)
         // Menambahkan kolom index/no sort
         ->addIndexColumn()
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,6 +15,22 @@
         @if (session('error'))
         <div class="alert-danger">{{ session('error') }}</div>
         @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
@@ -42,7 +58,10 @@ $(document).ready(function() {
         ajax: {
             url: "{{ url('user/list') }}",
             type: "POST",
-            dataType: "json"
+            dataType: "json",
+            data: function (d) {
+                d.level_id = $('#level_id').val();
+            }
         },
         columns: [
             { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
@@ -52,6 +71,11 @@ $(document).ready(function() {
             { data: "action", className: "", orderable: false, searchable: false }
         ]
     });
+
+    $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
+    });
+    
 });
 </script>
 @endpush
