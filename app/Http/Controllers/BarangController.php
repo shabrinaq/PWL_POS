2a185515aa106a667878d7e166902c09163e518e
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2025-05-25 18:16:42 +0700

    JS8 - Perbaiki Import Barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -58,34 +58,30 @@ class BarangController extends Controller
     {
         if ($request->ajax() || $request->wantsJson()) {
             $rules = [
-                'kategori_id' => 'required|integer',
-                'barang_kode' => 'required|string|min:3|unique:m_barang,barang_kode',
-                'barang_nama' => 'required|string|min:3|unique:m_barang,barang_nama',
-                'harga_beli' => 'required|integer',
-                'harga_jual' => 'required|integer'
+                'kategori_id' => ['required', 'integer', 'exists:m_kategori,kategori_id'],
+                'barang_kode' => ['required', 'min:3', 'max:20', 'unique:m_barang,barang_kode'],
+                'barang_nama' => ['required', 'string', 'max:100'],
+                'harga_beli' => ['required', 'numeric'],
+                'harga_jual' => ['required', 'numeric'],
             ];
-            $validator = Validator::make($request->all(), $rules);
 
+            $validator = Validator::make($request->all(), $rules);
             if ($validator->fails()) {
-                return response()->json([
-                    'status' => false,
+                return response()->json(['status' => false,
                     'message' => 'Validasi Gagal',
-                    'msgField' => $validator->errors()
+                    'msgField' => $validator->errors(),
                 ]);
             }
-            Barang::create([
-                'kategori_id' => $request->kategori_id,
-                'barang_kode' => $request->barang_kode,
-                'barang_nama' => $request->barang_nama,
-                'harga_beli' => $request->harga_beli,
-                'harga_jual' => $request->harga_jual
-            ]);
+
+            Barang::create($request->all());
+
             return response()->json([
                 'status' => true,
-                'message' => 'Data barang berhasil disimpan'
+                'message' => 'Data berhasil disimpan',
             ]);
         }
-        redirect('/');
+
+        return redirect('/');
     }
 
     // Menyimpan data barang baru
@@ -108,40 +104,26 @@ class BarangController extends Controller
         return redirect('/barang')->with('success', 'Data barang berhasil disimpan');
     }
 
-    public function list(Request $request) 
-    { 
-        $barang = Barang::select('barang_id', 'barang_kode', 'barang_nama', 
-            'harga_beli', 'harga_jual', 'kategori_id')->with('kategori'); 
+     public function list(Request $request)
+    {
+        $barang = Barang::select('barang_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'kategori_id')->with('kategori');
 
-        $kategori_id = $request->input('filter_kategori'); 
-        if (!empty($kategori_id)) { 
-            $barang->where('kategori_id', $kategori_id); 
-        } 
+        $kategori_id = $request->input('filter_kategori');
+        if (!empty($kategori_id)) {
+            $barang->where('kategori_id', $kategori_id);
+        }
 
-    return DataTables::of($barang) 
-        ->addIndexColumn() 
-        ->addColumn('aksi', function ($barang) { // menambahkan kolom aksi 
-            /*$btn = '<a href="'.url('/barang/' . $barang->barang_id).'" class="btn btn
-            info btn-sm">Detail</a> '; 
-            $btn .= '<a href="'.url('/barang/' . $barang->barang_id . 
-            '/edit').'"class="btn btn-warning btn-sm">Edit</a> '; 
-            $btn .= '<form class="d-inline-block" method="POST" action="'. 
-                url('/barang/'.$barang->barang_id).'">' 
-                . csrf_field() . method_field('DELETE') . 
-                '<button type="submit" class="btn btn-danger btn-sm" onclick="return 
-                confirm(\'Apakah Kita yakit menghapus data ini?\');">Hapus</button></form>';*/ 
-
-            $btn = '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id 
-                . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> '; 
-            $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang
-                ->barang_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> '; 
-            $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang
-                ->barang_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> '; 
-            return $btn; 
-        }) 
-        ->rawColumns(['aksi']) // ada teks html 
-        ->make(true); 
-    }   
+        return DataTables::of($barang)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($barang) {
+                $btn = '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
 
     // Menampilkan detail barang
     public function show(string $id)
