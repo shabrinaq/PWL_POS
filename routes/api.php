64f687c5774bd651a05da0f7c497753d51f152f7
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2025-05-30 21:13:46 +0700

    JS9_Prac 4 – Implementation of CRUD in RESTful API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+Route::get('level', [LevelController::class, 'index']);
+Route::post('level', [LevelController::class, 'store']);
+Route::get('level/{level}', [LevelController::class, 'show']);
+Route::put('level/{level}', [LevelController::class, 'update']);
+Route::delete('level/{level}', [LevelController::class, 'destroy']);
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
